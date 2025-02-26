4ef58ee14f8488adfc8498c021666ebe0b377830
LeonidMalin <leonid.malin@sensemitter.com>
2025-02-26 10:16:43 +0400

    Task 1525

---
+++ task_1525.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numSplits(self, s: str) -> int:
+        good_splits_count = 0
+        if not s:
+            return good_splits_count
+        left_split_letters_counter: dict[str, int] = {}
+        right_split_letters_counter: dict[str, int] = {}
+
+        # All letters are now in right split, so we count them all and store
+        # results
+        for letter in s:
+            right_split_letters_counter.setdefault(letter, 0)
+            right_split_letters_counter[letter] += 1
+
+        for idx, letter in enumerate(s, 1):
+            left, right = s[:idx], s[idx:]
+            if not left or not right:
+                continue
+
+            # Add to left counter
+            left_split_letters_counter.setdefault(letter, 0)
+            left_split_letters_counter[letter] += 1
+
+            # Remove from right counter
+            right_split_letters_counter[letter] -= 1
+            if right_split_letters_counter[letter] == 0:
+                del right_split_letters_counter[letter]
+
+            unique_letters_left = len(left_split_letters_counter)
+            unique_letters_right = len(right_split_letters_counter)
+            if unique_letters_left == unique_letters_right:
+                good_splits_count += 1
+
+        return good_splits_count
+
+
+sol = Solution()
+res = sol.numSplits("aaaaa")
+print(res)
